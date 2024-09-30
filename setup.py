57f4bfdae6373391eaf0ec74c229a884eb187951
Jesús Almansa <jtafernandez@gmail.com>
2024-09-30 11:23:27 +0000

    setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your_project_name',
+    version='0.1.0',
+    packages=find_packages(),
+  author='Your Name',
+    description='A description of your project'
+)
+
